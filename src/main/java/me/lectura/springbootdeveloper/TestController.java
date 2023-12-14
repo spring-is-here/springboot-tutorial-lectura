d56872917461d1b0468ba3b08bf35f3092955659
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2023-12-15 02:28:14 +0900

    Chapter 3

--- src/main/java/me/lectura/springbootdeveloper/TestController.java
+++ src/main/java/me/lectura/springbootdeveloper/TestController.java
@@ -1,12 +1,19 @@
 package me.lectura.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
-    @GetMapping("/test") // http://localhost:8080/test GET 접속
-    public String test(){
-        return "Hello, World!";
+    @Autowired
+    TestService testService;
+
+    @GetMapping("/test")
+    public List<Member> getAllMembers(){
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
