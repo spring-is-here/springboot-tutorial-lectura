c47a42874a23459005238092e3cbdcd157368b6b
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2023-12-16 17:16:50 +0900

    Chapter 5

--- src/main/java/me/lectura/springbootdeveloper/Member.java
+++ src/main/java/me/lectura/springbootdeveloper/Member.java
@@ -6,16 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // JPA가 관리하는 엔티티로 지정
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false) //  name이라는 not null 컬럼과 매핑
     private String name;
 }
--- src/main/java/me/lectura/springbootdeveloper/MemberRepository.java
+++ src/main/java/me/lectura/springbootdeveloper/MemberRepository.java
@@ -3,6 +3,6 @@ package me.lectura.springbootdeveloper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository // 엔티티 속 데이터를 조회, 변경할 때 사용하는 인터페이스
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
